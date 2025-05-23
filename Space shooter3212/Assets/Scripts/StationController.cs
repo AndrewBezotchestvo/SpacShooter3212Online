d6fcaeb94c30275e5a7ef75f8683d355c2a5b154
Andrew Bezotchestvo <03051820dr@gmail.com>
2025-05-23 22:53:51 +0800

    add enemy UI

---
+++ Space shooter3212/Assets/Scripts/StationController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StationController : MonoBehaviour
+{
+    [SerializeField] private float _stationHP;
+    [SerializeField] private float _rotationSpeed;
+
+    void Update()
+    {
+        transform.Rotate(0, _rotationSpeed * Time.deltaTime, 0);
+    }
+
+    public void GetDamage(float damage)
+    {
+        _stationHP -= damage;
+    }
+
+    public float GetHP()
+    {
+        return _stationHP;
+    }
+}
